c804c80c57878b259e3b3797a4b7e24f262b54ff
shinheeje <dbfk2637@naver.com>
2023-04-23 15:45:19 +0900

    Feat : Input component

--- react-styled-component2/src/App.jsx
+++ react-styled-component2/src/App.jsx
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Input from './components/Input';
 function App() {
   return (
     <div className="App">
-
+      <Input />
     </div>
   );
 }
