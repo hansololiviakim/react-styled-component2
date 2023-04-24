c7e3e13c352fc75439636accdb75a018dafae824
shinheeje <dbfk2637@naver.com>
2023-04-24 13:14:24 +0900

    Feat : Modal #4

--- react-styled-component2/src/App.jsx
+++ react-styled-component2/src/App.jsx
@@ -1,11 +1,12 @@
 import Input from './components/Input';
 import Buttons from './components/Buttons'
-
+import Modal from './components/Modal';
 function App() {
   return (
     <div className="App">
       <Input />
       <Buttons />
+      <Modal />
     </div>
   );
 }
