02e323fb9d2eaadaeb90c5511cacc14c3dc0e066
hansololiviakim <dev.hansolkim@gmail.com>
2023-04-24 16:41:09 +0900

    Feat: Final Release

---
+++ react-styled-final/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Buttons from './components/Buttons';
+import Input from './components/Input';
+import Modal from './components/Modal';
+import Select from './components/Select';
+
+function App() {
+  return (
+    <div>
+      <Buttons />
+      <Input />
+      <Modal />
+      <Select />
+    </div>
+  )
+}
+
+export default App
